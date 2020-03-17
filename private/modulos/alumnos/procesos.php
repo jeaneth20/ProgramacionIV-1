9693dd1f371d81e49423dac496f6cbd07d13441f
TCxDouglas <douglasalexander683@gmail.com>
2020-03-17 17:11:09 -0600

    procesos

---
+++ private/modulos/alumnos/procesos.php
@@ -0,0 +1,61 @@
+<?php
+
+include('../../Config/config.php');
+$alumno= new alumno($conexion);
+
+$proceso='';
+
+if(isset($_GET['proceso']) && strlen($_GET['proceso'])>0){
+    $proceso=$_GET['proceso'];
+}
+
+$alumno->$proceso($_GET['alumno']);
+print_r(json_encode($alumno->respuesta));
+
+class alumno{
+    private $datos=array(),$db;
+    public $respuesta=['msg'=>'correcto'];
+
+    public function __construct($db){
+        $this->db=$db;
+    }
+
+    public function recibirDatos($alumno){
+        $this->datos=json_decode($alumno, true);
+        $this->validar_datos();
+    }
+
+    private function validar_datos(){
+        if(empty($this->datos['codigo'])){
+            $this->respuesta['msg']='Por Favor Ingrese el codigo del estudiante';
+        
+        }
+        if(empty($this->datos['nombre'])){
+            $this->respuesta['msg']='Por Favor Ingrese el nombre del estudiante';
+
+        }
+        if(empty($this->datos['direccion'])){
+            $this->respuesta['msg']='Por Favor Ingrese la direccion del estudiante';
+
+        }
+        $this->almacenar_alumno();
+    }
+
+    private function almacenar_alumno(){
+        if($this->respuesta['msg']==='correcto'){
+            if($this->respuesta['accion']==="nuevo"){
+                $this->db->consultas('
+                INSERT INTO alumnos (codigo, nombre, direccion, telefono) VALUES(
+                    "'.$this->datos['codigo'] .'",
+                    "'.$this->datos['nombre'] .'",
+                    "'.$this->datos['direccion'] .'",
+                    "'.$this->datos['telefono'] .'"
+                    )
+                ');
+                $this->respuesta['msg']='Registro Insertado con Exito';
+            }
+        }
+    }
+}
+
+?>
